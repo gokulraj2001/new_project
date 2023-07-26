f0520d4c2eed19235c63805218976d038377dfba
Gokulraj V <gokulraj21122001@gmail.com>
2023-07-26 14:04:50 +0530

    File edited

--- Factorial.java
+++ Factorial.java
@@ -6,13 +6,13 @@ public static void main(String args[])
 Scanner s=new Scanner (System.in); 
 
 System.out.println("Enter a number");
-int b=s.nextInt();
+int a=s.nextInt();
  
 int i,fact=1;  
 
-for(i=1;i<=b;i++){    
+for(i=1;i<=a;i++){    
 fact=fact*i;    
 }    
-System.out.println("Factorial of "+b+" is: "+fact);    
+System.out.println("Factorial of "+a+" is: "+fact);    
 }  
 }  
